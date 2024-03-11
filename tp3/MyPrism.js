10d978909a53690ef11b65b878a36aa4e1272dab
Máximo Pereira <up202108887@up.pt>
2024-03-11 20:02:50 +0000

    added colors to tangram

---
+++ tp3/MyPrism.js
@@ -0,0 +1,37 @@
+import { CGFobject } from '../lib/CGF.js';
+
+/**
+ * MyPrism
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+export class MyPrism extends CGFobject {
+	constructor(scene, slices, stacks) {
+        super(scene);
+        this.slices = slices;
+        this.stacks = stacks;
+		this.initBuffers();
+	}
+	
+	initBuffers() {
+        this.vertices = [
+            -0.5, 0.5, 0,   // esquerda cima  0
+            -0.5, -0.5, 0,  // esquerda baixo 1
+            0.5, 0.5, 0,    // direita cima   2
+            0.5, -0.5, 0,   // direita baixo  3
+		];
+
+		//Counter-clockwise reference of vertices
+		this.indices = [
+            0, 1, 2,
+            1, 3, 2,
+		];
+
+		//The defined indices (and corresponding vertices)
+		//will be read in groups of three to draw triangles
+		this.primitiveType = this.scene.gl.TRIANGLES;
+
+		this.initGLBuffers();
+	}
+}
+
--- tp3/MyTangram.js
+++ tp3/MyTangram.js
@@ -1,4 +1,4 @@
-import {CGFobject} from '../lib/CGF.js';
+import { CGFobject } from '../lib/CGF.js';
 import { MyDiamond } from "./MyDiamond.js";
 import { MyTriangle } from "./MyTriangle.js";
 import { MyParallelogram } from "./MyParallelogram.js";
@@ -11,15 +11,21 @@ import { MyTriangleBig } from "./MyTriangleBig.js";
  * @param scene - Reference to MyScene object
  */
 export class MyTangram extends CGFobject {
-	constructor(scene) {
+	constructor(scene, diamondColor, parallelogramColor, BigTriangleBottomColor, BigTriangleTopColor, TriangleColor, SmallTriangleBottomColor, SmallTriangleTopColor) {
 		super(scene);
         this.diamond = new MyDiamond(this.scene);
         this.triangle = new MyTriangle(this.scene);
         this.parallelogram = new MyParallelogram(this.scene);
         this.triangleSmall = new MyTriangleSmall(this.scene);
         this.triangleBig = new MyTriangleBig(this.scene);
-	}
-	
+        this.diamondColor = diamondColor;
+        this.parallelogramColor = parallelogramColor;
+        this.BigTriangleBottomColor = BigTriangleBottomColor;
+        this.BigTriangleTopColor = BigTriangleTopColor;
+        this.TriangleColor = TriangleColor;
+        this.SmallTriangleBottomColor = SmallTriangleBottomColor;
+        this.SmallTriangleTopColor = SmallTriangleTopColor;
+    }
     
     display() {
         this.scene.pushMatrix();
@@ -34,47 +40,54 @@ export class MyTangram extends CGFobject {
             3.0, 2.0, 0.0, 1.0,
         ];
         this.scene.multMatrix(translacao);
+        this.diamondColor.apply();
         this.diamond.display();
         this.scene.popMatrix();
 
         //Parallellogram
         this.scene.pushMatrix();
         this.scene.rotate(Math.PI, 0, 1, 0);
-        this.scene.rotate(Math.PI/2, 0, 0, 1);
+        this.scene.rotate(Math.PI / 2, 0, 0, 1);
+        this.parallelogramColor.apply();
         this.parallelogram.display();
         this.scene.popMatrix();
 
         //Big Triangle
         this.scene.pushMatrix();
         this.scene.translate(1, 3, 0);
-        this.scene.rotate(-Math.PI/2, 0, 0, 1);
+        this.scene.rotate(-Math.PI / 2, 0, 0, 1);
+        this.BigTriangleTopColor.apply();
         this.triangleBig.display();
         this.scene.popMatrix();
 
         //Big Triangle
         this.scene.pushMatrix();
         this.scene.translate(3, 5, 0);
-        this.scene.rotate(Math.PI/2, 0, 0, 1);
+        this.scene.rotate(Math.PI / 2, 0, 0, 1);
+        this.BigTriangleBottomColor.apply();
         this.triangleBig.display();
         this.scene.popMatrix();
 
         //First Triangle
         this.scene.pushMatrix();
         this.scene.translate(2, 6, 0);
-        this.scene.rotate(-Math.PI/2, 0, 0, 1);
+        this.scene.rotate(-Math.PI / 2, 0, 0, 1);
+        this.TriangleColor.apply();
         this.triangle.display();
         this.scene.popMatrix();
 
         //Small Triangle
         this.scene.pushMatrix();
         this.scene.translate(2, 7, 0);
+        this.SmallTriangleTopColor.apply();
         this.triangleSmall.display();
         this.scene.popMatrix();
 
         //Small Triangle
         this.scene.pushMatrix();
         this.scene.translate(4, 1, 0);
-        this.scene.rotate(Math.PI/2, 0, 0, 1);
+        this.scene.rotate(Math.PI / 2, 0, 0, 1);
+        this.SmallTriangleBottomColor.apply();
         this.triangleSmall.display();
         this.scene.popMatrix();
         this.scene.popMatrix();
