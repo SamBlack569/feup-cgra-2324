cfcf9635bc55842d1eb71f0075fefaea6d6d34f6
Samuel Maciel <up202108697@edu.fe.up.pt>
2024-03-12 21:08:37 +0000

    Some chenges into MyPrism

--- tp3/MyPrism.js
+++ tp3/MyPrism.js
@@ -18,13 +18,15 @@ export class MyPrism extends CGFobject {
     display() {
         this.scene.pushMatrix();
 
-        for (let i = 0; i < Math.PI * 2; i += Math.PI * 2 / this.slices) {
-            this.scene.pushMatrix();
-            this.scene.rotate(i, 0, 0, 1);
-            this.scene.translate(0, 1, 0);
-            this.scene.rotate(-Math.PI / 2, 1, 0, 0);
-            this.quad.display();
-            this.scene.popMatrix();
+        for(let b = 0; b <= this.stacks; b += 1){
+            for (let i = 0; i < Math.PI * 2; i += Math.PI * 2 / this.slices) {
+                this.scene.pushMatrix();
+                this.scene.rotate(i, 0, 0, 1);
+                this.scene.translate(0, 0.866, b);
+                this.scene.rotate(-Math.PI / 2, 1, 0, 0);
+                this.quad.display();
+                this.scene.popMatrix();
+            }
         }
 
         this.scene.popMatrix();
