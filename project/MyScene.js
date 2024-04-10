564919cec5e9fe1b0fa1e54daa3b843103837375
Máximo Pereira <up202108887@up.pt>
2024-04-10 09:43:34 +0100

    created sphere with earth texture

--- project/MyScene.js
+++ project/MyScene.js
@@ -27,7 +27,7 @@ export class MyScene extends CGFscene {
     //Initialize scene objects
     this.axis = new CGFaxis(this);
     this.plane = new MyPlane(this,30);
-    this.sphere = new MySphere(this, 1, 30, 15);
+    this.sphere = new MySphere(this, 1, 25, 15);
 
     //Objects connected to MyInterface
     this.displayAxis = true;
